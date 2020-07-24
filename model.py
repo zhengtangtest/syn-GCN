b0375795ec56b3f41f88bfad493b7625d4167be7
Zheng Tang <oscartang1120@gmail.com>
2020-07-23 21:29:06 -0700

    v 0.1

---
+++ model.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import random
+from torch_geometric.nn import GCNConv
+
+SEED = 1234
+
+random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+device = torch.device('cpu')#"cuda" if torch.cuda.is_available() else "cpu")
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, embedding_size, syn_size, hidden_size, pretrained):
+        super(EncoderRNN, self).__init__()
+
+        self.embedding = nn.Embedding.from_pretrained(pretrained, freeze=False)
+        self.lemma_embedding = nn.Embedding(2, 5)
+        self.syn_embedding = nn.Embedding(syn_size, hidden_size)
+        
+        self.rnn = nn.LSTM(embedding_size + 5, hidden_size, bidirectional=True)
+
+        self.linear  = nn.Linear(hidden_size * 2,   hidden_size)
+        self.linear2 = nn.Linear(hidden_size + 610, hidden_size)
+
+        self.attn = nn.Linear(hidden_size, 1)
+
+    def forward(self, input, syn_labels, subj_pos, obj_pos, edge_index):
+        lemma = [1 if i in subj_pos or i in obj_pos else 0 for i in range(input.size(0))]
+        lemma = torch.tensor(lemma, dtype=torch.long, device=device).view(-1, 1)
+        lemma_embeded = self.lemma_embedding(lemma).view(-1, 1, 5)
+        
+        embedded = self.embedding(input).view(-1, 1, 300)
+        embedded = torch.cat((embedded, lemma_embeded), dim=2)
+        
+        syn_embedded         = self.syn_embedding(syn_labels).view(syn_labels.size(0), -1)
+        first_word_embedded  = embedded[edge_index[0],:,:].view(syn_labels.size(0), -1)
+        second_word_embedded = embedded[edge_index[1],:,:].view(syn_labels.size(0), -1)
+        syn_embedded         = torch.cat((syn_embedded, first_word_embedded, second_word_embedded), 1)
+        syn_embedded         = self.linear2(syn_embedded)
+
+        output, hidden = self.rnn(embedded)
+        output  = self.linear(output)
+        outputs = output.view(input.size(0), -1)
+        
+        subj_vec = outputs[subj_pos[0]:subj_pos[-1]+1]
+        obj_vec = outputs[obj_pos[0]:obj_pos[-1]+1]
+        subj, sw = self.event_summary(subj_vec)
+        obj, ow = self.event_summary(obj_vec)
+        return outputs, subj, obj, sw, ow, syn_embedded
+
+    def event_summary(self, event):
+        attn_weights = F.softmax(torch.t(self.attn(event)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 event.unsqueeze(0))
+        return attn_applied[0, 0], attn_weights
+
+class Classifier(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Classifier, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.attn = nn.Linear(input_size, hidden_size, bias=False)
+        self.gcn = GCNConv(hidden_size, hidden_size)
+
+        self.out = nn.Linear(hidden_size * 3, output_size)
+        self.softmax = nn.LogSoftmax(dim=0)
+
+    def forward(self, encoder_outputs, syn_embeddeds, subj, obj, edge_index):
+        edge_weights = F.softmax(
+            torch.mm(
+                self.attn(encoder_outputs[0].view( 1,-1)), torch.t(syn_embeddeds)
+                )
+            , dim=1)
+        edge_weights = edge_weights.squeeze(0)
+        outputs = self.gcn(encoder_outputs, edge_index, edge_weights)
+        output = torch.cat((outputs[0], subj, obj))
+        output = self.softmax(self.out(output))
+        return output.unsqueeze(0)
---
+++ train.py
@@ -0,0 +1,101 @@
+from data_utils import *
+from score import score
+import os
+
+def train(training_data, encoder, classifier, encoder_optimizer, classifier_optimizer, criterion):
+
+    encoder.train()
+    classifier.train()
+
+    for datapoint in training_data:
+        
+        relation    = torch.tensor([datapoint[0]], dtype=torch.long, device=device)
+        sentence    = tensorFromIndexes(datapoint[1])
+        edge_index  = torch.tensor(datapoint[2], dtype=torch.long, device=device)
+        label_index = tensorFromIndexes(datapoint[3])
+        entity_subj = datapoint[4]
+        entity_obj  = datapoint[5]
+        type_subj   = datapoint[6]
+        type_obj    = datapoint[7]
+
+        outputs, subj, obj, sw, ow, syn_embedded = encoder(sentence, label_index,
+            entity_subj, entity_obj, edge_index)
+
+        predict = classifier(outputs, syn_embedded, subj, obj, edge_index)
+
+        loss = criterion(predict, relation)
+
+        loss.backward()
+
+        clipping_value = 1#arbitrary number of your choosing
+        torch.nn.utils.clip_grad_norm_(encoder.parameters(), clipping_value)
+        torch.nn.utils.clip_grad_norm_(classifier.parameters(), clipping_value)
+
+        encoder_optimizer.step()
+        classifier_optimizer.step()
+
+def eval(dev_data, encoder, classifier):
+
+    encoder.eval()
+    classifier.eval()
+    
+    key        = list()
+    prediction = list()
+
+    with torch.no_grad():
+
+        for datapoint in dev_data:
+            
+            relation    = id2relation[datapoint[0]]
+            sentence    = tensorFromIndexes(datapoint[1])
+            edge_index  = torch.tensor(datapoint[2], dtype=torch.long, device=device)
+            label_index = tensorFromIndexes(datapoint[3])
+            entity_subj = datapoint[4]
+            entity_obj  = datapoint[5]
+            type_subj   = datapoint[6]
+            type_obj    = datapoint[7]
+
+            key.append(relation)
+
+            outputs, subj, obj, sw, ow, syn_embedded = encoder(sentence, label_index,
+                entity_subj, entity_obj, edge_index)
+
+            predict = classifier(outputs, syn_embedded, subj, obj, edge_index)
+
+            topv, topi = predict.topk(1)
+
+            prediction.append(id2relation[topi.item()])
+
+    score(key, prediction, verbose=True)
+
+
+
+if __name__ == '__main__':
+
+    input_lang, dep_lang, training_data = load_data('tacred/data/json/train.json')
+
+    dev_data = load_data('tacred/data/json/dev.json', input_lang, dep_lang)
+
+    embeds, embedding_size = load_embeddings("glove.840B.300d.txt", input_lang)
+    embeds = torch.FloatTensor(embeds)
+    hidden_size = 100
+    learning_rate = 0.000001
+    criterion = nn.NLLLoss()
+
+    encoder    = EncoderRNN(input_lang.n_words, embedding_size, 
+        dep_lang.n_words, hidden_size, embeds).to(device)
+    classifier = Classifier(hidden_size, hidden_size, len(id2relation)).to(device)
+
+    encoder_optimizer    = optim.Adam(encoder.parameters(), lr=learning_rate)
+    classifier_optimizer = optim.Adam(classifier.parameters(), lr=learning_rate)
+
+    for epoch in range(100):
+        random.shuffle(training_data)
+        train(training_data, encoder, classifier, encoder_optimizer, classifier_optimizer, criterion)
+
+        eval(dev_data, encoder, classifier)
+
+        os.mkdir("model/%d"%epoch)
+        PATH = "model/%d"%epoch
+        torch.save(encoder, PATH+"/encoder")
+        torch.save(classifier, PATH+"/classifier")
