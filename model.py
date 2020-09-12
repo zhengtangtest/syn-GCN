926c6307dd2cc5120c9364685ff13ae7dde63049
Zheng Tang <oscartang1120@gmail.com>
2020-09-11 18:41:17 -0700

    update syn GCN

--- model.py
+++ model.py
@@ -136,7 +136,7 @@ class SynGCN(nn.Module):
                     padding_idx=constant.PAD_ID)
             self.gat = GATConv((2*opt['hidden_dim'], 2*opt['hidden_dim']+opt['deprel_dim']), 2*opt['hidden_dim'])
 
-        if opt['ee']:
+        if opt['ee'] or opt['sgcn']:
             self.linear = nn.Linear(4*opt['hidden_dim'], opt['num_class'])
         else:
             self.linear = nn.Linear(2*opt['hidden_dim'], opt['num_class'])
@@ -212,31 +212,30 @@ class SynGCN(nn.Module):
 
         if self.opt['sgcn']:
             deprel = self.deprel_emb(deprel)
-            weights = self.attn(deprel, d_masks, outputs[:,0,:]).view(-1)
-            weights = weights[weights.nonzero()].squeeze(1)
+            subj_avg = ((~subj_mask).float())/(~subj_mask).float().sum(-1).view(-1, 1)
+            obj_avg = ((~obj_mask).float())/(~obj_mask).float().sum(-1).view(-1, 1)
+            subj = subj_avg.unsqueeze(1).bmm(outputs).squeeze(1)
+            obj  = obj_avg.unsqueeze(1).bmm(outputs).squeeze(1)
+
+            s_weights = self.attn(deprel, d_masks, subj).view(-1)
+            o_weights = self.attn(deprel, d_masks, obj).view(-1)
+            s_weights = s_weights[s_weights.nonzero()].squeeze(1)
+            o_weights = o_weights[o_weights.nonzero()].squeeze(1)
+
             outputs = outputs.reshape(s_len*batch_size, -1)
-            for i in range(self.opt['num_layers']):
-                outputs = self.sgcn(outputs, edge_index, weights)
-            outputs = outputs.reshape(batch_size, s_len, -1)
+            
+            s_outputs = self.sgcn(outputs, edge_index, s_weights)
+            o_outputs = self.sgcn(outputs, edge_index, o_weights)
+            s_outputs = s_outputs.reshape(batch_size, s_len, -1)
+            o_outputs = o_outputs.reshape(batch_size, s_len, -1)
 
-            if self.opt['ee']:
-                if self.opt['e_attn']:
-                    subj_weights = self.entity_attn(outputs, subj_mask, outputs[:,0,:])
-                    obj_weights  = self.entity_attn(outputs, obj_mask, outputs[:,0,:])
-                else:
-                    # Average
-                    subj_weights = ((~subj_mask).float())/(~subj_mask).float().sum(-1).view(-1, 1)
-                    obj_weights = ((~obj_mask).float())/(~obj_mask).float().sum(-1).view(-1, 1)
+            subj = subj_avg.unsqueeze(1).bmm(s_outputs).squeeze(1)
+            obj  = obj_avg.unsqueeze(1).bmm(o_outputs).squeeze(1)
 
-                subj = subj_weights.unsqueeze(1).bmm(outputs).squeeze(1)
-                obj  = obj_weights.unsqueeze(1).bmm(outputs).squeeze(1)
+            final_hidden = self.drop(torch.cat([subj, obj] , dim=1))
 
-                final_hidden = self.drop(torch.cat([subj, obj] , dim=1))
 
-            else:
-                final_hidden = outputs[:,0,:]
-
-        if self.opt['gat']:
+        elif self.opt['gat']:
             deprel    = self.deprel_emb(deprel)
             outputs_t = torch.cat([outputs, deprel], dim=2)
             outputs   = outputs.reshape(s_len*batch_size, -1)
