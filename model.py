17781b16d612e8cfc1670dc2ef3916ede1a7595b
Zheng Tang <oscartang1120@gmail.com>
2020-09-13 22:47:42 -0700

    Update model.py

--- model.py
+++ model.py
@@ -223,7 +223,7 @@ class SynGCN(nn.Module):
             outputs = outputs.reshape(s_len*batch_size, -1)
             
             outputs = self.sgcn(outputs, edge_index, weights)
-            outputs = s_outputs.reshape(batch_size, s_len, -1)
+            outputs = outputs.reshape(batch_size, s_len, -1)
 
             subj = subj_avg.unsqueeze(1).bmm(outputs).squeeze(1)
             obj  = obj_avg.unsqueeze(1).bmm(outputs).squeeze(1)
