c52d7e2ed1aef3512649e7f59d9480f70be6394b
Zheng Tang <oscartang1120@gmail.com>
2020-09-23 16:35:48 -0700

    Update model.py

--- model.py
+++ model.py
@@ -121,8 +121,8 @@ class SynGCN(nn.Module):
             self.deprel_emb = nn.Embedding(len(constant.DEPREL_TO_ID), opt['deprel_dim'],
                     padding_idx=constant.PAD_ID)
             self.attn = Attention(opt['deprel_dim'], 2*opt['hidden_dim'])
-            self.sgcn = GCNConv(2*opt['hidden_dim'], opt['hidden_dim'])
-            self.sgcn2 = GCNConv(opt['hidden_dim'], opt['hidden_dim'])
+            self.sgcn = GCNConv(2*opt['hidden_dim'], 2*opt['hidden_dim'])
+            self.sgcn2 = GCNConv(2*opt['hidden_dim'], opt['hidden_dim'])
 
         if opt['pattn']:
             self.attn_layer = PositionAwareAttention(2*opt['hidden_dim'],
