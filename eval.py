87f467dbdb7f614917a00f50ca89f8c300f8cdbb
Zheng Tang <oscartang1120@gmail.com>
2020-09-25 02:58:16 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,13 +1,18 @@
 """
 Run evaluation with saved models.
 """
+
+import os
 import random
 import argparse
-from tqdm import tqdm
+import pickle
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
 
-from data.loader import DataLoader
-from model.trainer import GCNTrainer
+from loader import BatchLoader
+from model import RelationModel
 from utils import torch_utils, scorer, constant, helper
 from utils.vocab import Vocab
 
@@ -16,6 +21,7 @@ parser.add_argument('model_dir', type=str, help='Directory of the model.')
 parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
 parser.add_argument('--data_dir', type=str, default='dataset/tacred')
 parser.add_argument('--dataset', type=str, default='test', help="Evaluate on dev or test.")
+parser.add_argument('--out', type=str, default='', help="Save model predictions to this dir.")
 
 parser.add_argument('--seed', type=int, default=1234)
 parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available())
@@ -23,7 +29,7 @@ parser.add_argument('--cpu', action='store_true')
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
-random.seed(1234)
+random.seed(args.seed)
 if args.cpu:
     args.cuda = False
 elif args.cuda:
@@ -33,8 +39,8 @@ elif args.cuda:
 model_file = args.model_dir + '/' + args.model
 print("Loading model from {}".format(model_file))
 opt = torch_utils.load_config(model_file)
-trainer = GCNTrainer(opt)
-trainer.load(model_file)
+model = RelationModel(opt)
+model.load(model_file)
 
 # load vocab
 vocab_file = args.model_dir + '/vocab.pkl'
@@ -44,7 +50,10 @@ assert opt['vocab_size'] == vocab.size, "Vocab size must match that in the saved
 # load data
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 print("Loading data from {} with batch size {}...".format(data_file, opt['batch_size']))
-batch = DataLoader(data_file, opt['batch_size'], opt, vocab, evaluation=True)
+eval_batch = BatchLoader(data_file, opt['batch_size'], opt, vocab, evaluation=True)
+
+helper.print_config(opt)
+id2label = dict([(v,k) for k,v in constant.LABEL_TO_ID.items()])
 
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
@@ -52,7 +61,7 @@ id2label = dict([(v,k) for k,v in label2id.items()])
 
 predictions = []
 all_probs = []
-batch_iter = tqdm(batch)
+batch_iter = tqdm(eval_batch)
 for i, b in enumerate(batch_iter):
     preds, probs, _ = trainer.predict(b)
     predictions += preds
