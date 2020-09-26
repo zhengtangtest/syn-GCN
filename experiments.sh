ea49270492d57d5ed7872eb8867e5905297ad806
Zheng Tang <oscartang1120@gmail.com>
2020-09-26 00:20:32 -0700

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -1,15 +1,11 @@
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 10 --info "S GCN" --optim adagrad --sgcn --seed $1 > log_10 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 11 --info "S GCN, entity average" --optim adagrad --sgcn --ee --seed $1 > log_11 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 12 --info "S GCN, entity attention" --optim adagrad --sgcn --ee --e_attn --seed $1 > log_12 &
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 00 --info "gcn, no label" --lr 0.3 --gcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 1234 --save_dir ./new_models > log00
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 01 --info "gcn, no label" --lr 0.3 --gcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 0 --save_dir ./new_models > log01
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 02 --info "gcn, no label" --lr 0.3 --gcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 1 --save_dir ./new_models > log02
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 03 --info "gcn, no label" --lr 0.3 --gcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 2 --save_dir ./new_models > log03
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 04 --info "gcn, no label" --lr 0.3 --gcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 9999 --save_dir ./new_models > log04
 wait
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 13 --info "R GCN" --optim adagrad --rgcn --seed $1 > log_13 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 14 --info "R GCN, entity average" --optim adagrad --rgcn --ee --seed $1 > log_14 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 15 --info "R GCN, entity attention" --optim adagrad --rgcn --ee --e_attn --seed $1 > log_15 &
-wait
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 16 --info "Vanilla GCN" --optim adagrad --gcn --seed $1 > log_16 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 17 --info "Vanilla GCN, entity average" --optim adagrad --gcn --ee --seed $1 > log_17 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 18 --info "Vanilla GCN, entity attention" --optim adagrad --gcn --ee --e_attn --seed $1 > log_18 &
-wait
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 19 --info "LSTM" --optim adagrad --seed $1 > log_19 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 110 --info "LSTM, entity average" --optim adagrad --ee --seed $1 > log_110 &
-python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 111 --info "LSTM, entity attention" --optim adagrad --ee --e_attn --seed $1 > log_111 &
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 10 --info "gcn, with label" --lr 0.3 --sgcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 1234 --save_dir ./new_models > log10
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 11 --info "gcn, with label" --lr 0.3 --sgcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 0 --save_dir ./new_models > log11
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 12 --info "gcn, with label" --lr 0.3 --sgcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 1 --save_dir ./new_models > log12
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 13 --info "gcn, with label" --lr 0.3 --sgcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 2 --save_dir ./new_models > log13
+python train.py --data_dir tacred/data/json/ --vocab_dir vocab --id 14 --info "gcn, with label" --lr 0.3 --sgcn --batch_size 50 --pooling_l2 0.003 --prune_k 1 --mlp_layers 2 --seed 9999 --save_dir ./new_models > log14
