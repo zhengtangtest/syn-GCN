b7f93a3f5daa2fea23f03c40c0803a62508cc4b9
Zheng Tang <oscartang1120@gmail.com>
2020-10-25 01:31:32 -0700

    Update model.py

--- model.py
+++ model.py
@@ -448,6 +448,7 @@ class Decoder(nn.Module):
     def forward(self, input, masks, last_hidden, encoder_outputs):
 
         # Get the embedding of the current input word (last output word)
+        print (input.get_device())
         embedded = self.embed(input).unsqueeze(0)  # (1,B,N)
         embedded = self.dropout(embedded)
 
