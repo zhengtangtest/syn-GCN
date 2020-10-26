b5be9f0083d1e02d3e5ecf858a451be06e3c70fa
Zheng Tang <oscartang1120@gmail.com>
2020-10-26 00:41:02 -0700

    Update train.py

--- train.py
+++ train.py
@@ -167,8 +167,7 @@ for epoch in range(1, opt['num_epoch']+1):
         preds, _, outputs, loss = model.predict(batch, True)
         predictions += preds
         dev_loss += loss
-        rules = batch.rule
-        print (rules.size())
+        rules = batch.rule.view(batch_size, -1)
         for i in rules.size(1):
             output = outputs.transpose(0, 1)[i]
             print ([vocab.id2rule[int(r)] for r in rules[i].tolist()])
