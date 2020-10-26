95122d826391cf4892cd587567b7b95816114db1
Zheng Tang <oscartang1120@gmail.com>
2020-10-26 00:18:57 -0700

    Update train.py

--- train.py
+++ train.py
@@ -167,8 +167,8 @@ for epoch in range(1, opt['num_epoch']+1):
         preds, _, outputs, loss = model.predict(batch, True)
         predictions += preds
         dev_loss += loss
-        print (outputs.size())
-        print (outputs.transpose(0, 1))
+        for output in outputs.transpose(0, 1):
+            print ([id2rule[r] for r in output])
     predictions = [id2label[p] for p in predictions]
     dev_p, dev_r, dev_f1 = scorer.score(dev_batch.gold(), predictions)
     
